9318340b1a376df9bb56c4532972e06b7bfed70b
ywerling <ywerling@hotmail.de>
2025-02-07 10:42:00 +0100

    First version

---
+++ VSCodeSetup/GoSources/firstproject.go
@@ -0,0 +1,15 @@
+// package main
+//
+// import {
+// "fmt"
+// }
+//
+// func main() {
+// fmt.Println("Welcome to the challenge")
+// }
+
+package GoSources
+
+func BestMascot() string {
+	return "Best"
+}
---
+++ VSCodeSetup/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/go-demo-1/GoSources"
+)
+
+func main() {
+	fmt.Println(GoSources.BestMascot())
+}
